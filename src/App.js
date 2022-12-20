947faa392f191f610c517eb8c75232777badf264
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2022-12-20 16:19:04 +0700

    finish lab jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// Lab 1
+// function App() {
+//   const fullName = 'Mark Sookerbrek';
+//   const birthYear = 2000;
+//   return (
+//     <div>
+//       <h1>Full Name: {fullName}</h1>
+//       <p>Age: {2022 - birthYear}</p>
+//     </div>
+//   );
+// }
 
+// Lab 2
+// function App() {
+//   const height = 1.6;
+//   const weight = 50;
+//   const calcBMI = (h, w) => w / h ** 2;
+//   return <h1>{calcBMI(height, weight)}</h1>;
+// }
+
+// Lab 3
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label htmlFor="username">Username</label>
+      <input className="username" id="username" name="username" />
     </div>
   );
 }
