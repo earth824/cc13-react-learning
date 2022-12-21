f75284be445bb0663eb06c3ea33a9ab6740d426e
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2022-12-21 09:32:22 +0700

    finish lab component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+// Lab 3
+// function App() {
+//   return <ProductItem name="Pepsi" price={50} description="Pepsi is da best" />;
+// }
 
+// function ProductItem(props) {
+//   console.log(typeof props.price);
+//   return (
+//     <div>
+//       <h1>{props.name}</h1>
+//       <h2>{props.price}</h2>
+//       <p>{props.description}</p>
+//     </div>
+//   );
+// }
+
+// function ProductItem({ name, price, description }) {
+//   console.log(typeof price);
+//   return (
+//     <div>
+//       <h1>{name}</h1>
+//       <h2>{price}</h2>
+//       <p>{description}</p>
+//     </div>
+//   );
+// }
+
+// Lab 4
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ProductItem
+      product={{ name: 'Pepsi', price: 50, description: 'Pepsi is da best' }}
+    />
+  );
+}
+
+// function ProductItem(props) {
+//   return (
+//     <div>
+//       <h1>{props.product.name}</h1>
+//       <h2>{props.product.price}</h2>
+//       <p>{props.product.description}</p>
+//     </div>
+//   );
+// }
+
+function ProductItem({ product: { name, price, description } }) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{price}</h2>
+      <p>{description}</p>
     </div>
   );
 }
